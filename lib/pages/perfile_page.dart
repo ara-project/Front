27ad5e9797ccbf8996c0f92a6dec0b8b4f0ae6a2
DaniPadi <danipadi1216@gmail.com>
2024-04-16 08:22:53 -0500

    Daniel

--- lib/pages/perfile_page.dart
+++ lib/pages/perfile_page.dart
@@ -65,7 +65,6 @@ class Perfil extends StatelessWidget {
                             ],
                       ),
                   ],),
-                  Row()
             ],
           ),
         ),    
