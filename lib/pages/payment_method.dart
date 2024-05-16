6acd1ff31e46ea923e6783651949bfb3f6343128
Jose Larrada <jalarrada@unicesar.edu.co>
2024-05-15 23:16:25 -0500

    Vista para seleccionar metodo de pago completa

--- lib/pages/payment_method.dart
+++ lib/pages/payment_method.dart
@@ -25,8 +25,8 @@ class _PaymentMethodState extends State<PaymentMethod> {
         appBar: AppBar(
           title: Text('Seleccionar metodo de compra'),
           leading: BackButton(),
-          backgroundColor: Colors.transparent,
-          foregroundColor: Colors.black,
+          backgroundColor: Color(0xFFDB3022),
+          foregroundColor: Colors.white,
           elevation: 0,
           centerTitle: true,
         ),
@@ -40,7 +40,6 @@ class _PaymentMethodState extends State<PaymentMethod> {
                   Container(
                     width: sise.width,
                     height: 35,
-                    margin: EdgeInsets.only(right: 20),
                     decoration: BoxDecoration(
                         border: _type == 1
                             ? Border.all(width: 1, color: Color(0xFFDB3022))
@@ -48,39 +47,226 @@ class _PaymentMethodState extends State<PaymentMethod> {
                         borderRadius: BorderRadius.circular(5),
                         color: Colors.transparent),
                     child: Center(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Row(
-                            children: [
-                              Radio(
-                                value: 1,
-                                groupValue: _type,
-                                onChanged: _handleRadio,
-                                activeColor: Color(0xFFDB3022),
-                              ),
-                              Text(
-                                "Nequi",
-                                style: _type == 1
-                                    ? TextStyle(
-                                        fontSize: 15,
-                                        fontWeight: FontWeight.w500,
-                                        color: Colors.black)
-                                    : TextStyle(
-                                        fontSize: 15,
-                                        fontWeight: FontWeight.w500,
-                                        color: Colors.grey),
-                              ),
-                            ],
-                          ),
-                          Image.asset(
-                            "assets/nequi_logo.png",
-                            width: 70,
-                            height: 70,
-                            fit: BoxFit.cover,
-                          ),
-                        ],
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 20),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                Radio(
+                                  value: 1,
+                                  groupValue: _type,
+                                  onChanged: _handleRadio,
+                                  activeColor: Color(0xFFDB3022),
+                                ),
+                                Text(
+                                  "Nequi",
+                                  style: _type == 1
+                                      ? TextStyle(
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.w500,
+                                          color: Colors.black)
+                                      : TextStyle(
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.w500,
+                                          color: Colors.grey),
+                                ),
+                              ],
+                            ),
+                            Image.asset(
+                              "assets/nequi_logo.png",
+                              width: 30,
+                              fit: BoxFit.cover,
+                            ),
+                          ],
+                        ),
                       ),
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  Container(
+                    width: sise.width,
+                    height: 35,
+                    decoration: BoxDecoration(
+                        border: _type == 2
+                            ? Border.all(width: 1, color: Color(0xFFDB3022))
+                            : Border.all(width: 0.3, color: Colors.grey),
+                        borderRadius: BorderRadius.circular(5),
+                        color: Colors.transparent),
+                    child: Center(
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 20),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                Radio(
+                                  value: 2,
+                                  groupValue: _type,
+                                  onChanged: _handleRadio,
+                                  activeColor: Color(0xFFDB3022),
+                                ),
+                                Text(
+                                  "Tarjeta de credito",
+                                  style: _type == 2
+                                      ? TextStyle(
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.w500,
+                                          color: Colors.black)
+                                      : TextStyle(
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.w500,
+                                          color: Colors.grey),
+                                ),
+                              ],
+                            ),
+                            Spacer(),
+                            Image.asset(
+                              "assets/Visa_Inc._logo.png",
+                              width: 30,
+                            ),
+                            SizedBox(width: 8),
+                            Image.asset(
+                              "assets/MasterCard_Logo.png",
+                              width: 30,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  Container(
+                    width: sise.width,
+                    height: 35,
+                    decoration: BoxDecoration(
+                        border: _type == 3
+                            ? Border.all(width: 1, color: Color(0xFFDB3022))
+                            : Border.all(width: 0.3, color: Colors.grey),
+                        borderRadius: BorderRadius.circular(5),
+                        color: Colors.transparent),
+                    child: Center(
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 20),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                Radio(
+                                  value: 3,
+                                  groupValue: _type,
+                                  onChanged: _handleRadio,
+                                  activeColor: Color(0xFFDB3022),
+                                ),
+                                Text(
+                                  "Bancolombia A la mano",
+                                  style: _type == 3
+                                      ? TextStyle(
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.w500,
+                                          color: Colors.black)
+                                      : TextStyle(
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.w500,
+                                          color: Colors.grey),
+                                ),
+                              ],
+                            ),
+                            Image.asset(
+                              "assets/LogoBancolombia.png",
+                              width: 30,
+                              fit: BoxFit.cover,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 100),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Sub-Total",
+                        style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.grey),
+                      ),
+                      Text(
+                        "200.000",
+                        style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.grey),
+                      )
+                    ],
+                  ),
+                  SizedBox(height: 15),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Costo de envio",
+                        style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.grey),
+                      ),
+                      Text(
+                        "5000",
+                        style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.grey),
+                      )
+                    ],
+                  ),
+                  Divider(
+                    height: 30,
+                    color: Colors.black,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Total de compra",
+                        style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.grey),
+                      ),
+                      Text(
+                        "205.000",
+                        style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w700,
+                            color: Color(0xFFDB3022)),
+                      )
+                    ],
+                  ),
+                  SizedBox(height: 70),
+                  TextButton(
+                    onPressed: () => {print("Hola")},
+                    style: TextButton.styleFrom(
+                      backgroundColor: Color(0xFFDB3022),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(
+                            10), // Define la forma del borde
+                      ),
+                    ),
+                    child: Text(
+                      "Continuar pago",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold),
                     ),
                   )
                 ],
